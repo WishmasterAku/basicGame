5fb3d1fc2e872387a4076a3b3f0a91c47bde0c38
Wishymashy <amoralesmm@yahoo.com>
2022-11-09 15:46:44 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+playerName = (input("Hello please enter your name "))
+gameActive = (input("Type yes to Start Game ").lower())
+if gameActive != "yes":
+    print("Shutting Down")
+    quit()
+print("Let the games begin!")

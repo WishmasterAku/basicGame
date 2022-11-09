c146fac8eb0f471f075b1d5630325b4f9c6b1a28
Wishymashy <amoralesmm@yahoo.com>
2022-11-09 15:59:50 -0600

    Game options

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ if gameActive != "yes":
     print("Shutting Down")
     quit()
 print("Let the games begin!")
+print("Please choose which game you like to play")
+print("1. Quiz Game\n2. Guess the Number\n3. Rock Paper Scissors\n4. Adventurer Games\n5. Zelda Python")
+gameOption = input("")

f458903ecbda1d0027f40f626a7c02dd939b391e
Wishymashy <amoralesmm@yahoo.com>
2022-11-10 14:40:00 -0600

    Added simple computer quiz game

--- main.py
+++ main.py
@@ -6,4 +6,37 @@ if gameActive != "yes":
 print("Let the games begin!")
 print("Please choose which game you like to play")
 print("1. Quiz Game\n2. Guess the Number\n3. Rock Paper Scissors\n4. Adventurer Games\n5. Zelda Python")
-gameOption = input("")
+gameOption = input("1")
+
+print("Computer Quiz Game")
+
+results = 0
+answer = (input("What does CPU stand for? ").lower())
+if answer == "central processing unit":
+    print("You are correct")
+    results += 1
+else:
+    print("Sorry wrong answer")
+    
+answer = (input("What does GPU stand for? ").lower())
+if answer == "graphics processing unit":
+    print("You are correct")
+    results += 1
+else:
+    print("Sorry wrong answer")
+    
+answer = (input("What does RAM stand for? ").lower())
+if answer == "random access memory":
+    print("You are correct")
+    results += 1
+else:
+    print("Sorry wrong answer")
+    
+answer = (input("What does PSU stand for? ").lower())
+if answer == "power supply unit":
+    print("You are correct")
+    results += 1
+else:
+    print("Sorry wrong answer")
+    
+print(f"{playerName}'s total points is {results}")
